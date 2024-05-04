bf1329a37cb078512dd21765553cbf49f9ef52ce
JacoTome <jacotomelleri@hotmail.com>
2024-05-04 16:21:20 +0200

    First Commit

---
+++ src/test/java/musico/services/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package musico.services.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
